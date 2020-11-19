31d03033560d521996c81c9a9254b1b0d5922984
Tobias Ribizel <ribizel@kit.edu>
2020-11-19 15:22:24 +0100

    Update ilu.cpp

--- core/factorization/ilu.cpp
+++ core/factorization/ilu.cpp
@@ -1,7 +1,6 @@
 /*******************************<GINKGO LICENSE>******************************
 Copyright (c) 2017-2020, the Ginkgo authors
 All rights reserved.
-
 Redistribution and use in source and binary forms, with or without
 modification, are permitted provided that the following conditions
 are met:
