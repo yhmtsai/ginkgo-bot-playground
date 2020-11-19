add7dbe0d50e80fb2d95d08b01bbcd3f7bbe9d93
Tobias Ribizel <ribizel@kit.edu>
2020-11-19 16:02:33 +0100

    improve

--- .github/rebase.sh
+++ .github/rebase.sh
@@ -6,10 +6,10 @@ if [[ "$PR_MERGED" == "true" ]]; then
   bot_error "PR already merged!"
 fi
 
-git remote set-url origin "$BASE_URL"
+git remote add base "$BASE_URL"
 git remote add fork "$HEAD_URL"
 
-git fetch origin $BASE_BRANCH
+git fetch base $BASE_BRANCH
 git fetch fork $HEAD_BRANCH
 
 git config user.email "$USER_EMAIL"
@@ -18,15 +18,8 @@ git config user.name "$USER_NAME"
 LOCAL_BRANCH=rebase-tmp-$HEAD_BRANCH
 git checkout -b $LOCAL_BRANCH fork/$HEAD_BRANCH
 
-git log 2>&1 || head -n20
-echo; echo;
-git log $LOCAL_BRANCH 2>&1 || head -n20
-echo; echo;
-git log origin/$BASE_BRANCH 2>&1 || head -n20
-echo; echo;
-
 # do the rebase
-git rebase origin/$BASE_BRANCH 2>&1 || bot_error "Rebasing failed"
+git rebase base/$BASE_BRANCH 2>&1 || bot_error "Rebasing failed"
 
 # push back
 git push --force-with-lease fork $LOCAL_BRANCH:$HEAD_BRANCH 2>&1 || bot_error "Cannot push rebased branch, are edits for maintainers allowed?"
