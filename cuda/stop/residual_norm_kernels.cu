16a1335b08583a078be642a8d07c061e1f369fd0
Yuhsiang M. Tsai <yhmtsai@gmail.com>
2020-11-20 14:55:47 +0100

    dummy change

--- cuda/stop/residual_norm_kernels.cu
+++ cuda/stop/residual_norm_kernels.cu
@@ -38,6 +38,15 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #include <ginkgo/core/stop/residual_norm.hpp>
 
 
+
+
+
+
+
+
+
+
+
 #include "cuda/base/math.hpp"
 #include "cuda/base/types.hpp"
 #include "cuda/components/thread_ids.cuh"
